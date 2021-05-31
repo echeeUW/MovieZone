112f38b9d38ea1ba36a8ea1b0478aabda85ec4a2
Eric Chee <eric.chee@hulu.com>
2021-05-31 08:18:03 -0700

    Added sample code how to structure DataRepository

--- app/src/main/java/com/info448/moviezone/MainActivity.kt
+++ app/src/main/java/com/info448/moviezone/MainActivity.kt
@@ -5,29 +5,53 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import coil.load
+import com.info448.moviezone.databinding.ActivityMainBinding
 import com.info448.moviezone.model.MovieDetail
 import com.info448.moviezone.model.MovieImage
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val movieZoneApplication: MovieZoneApplication by lazy {application as MovieZoneApplication}
     private val dataRepository by lazy {movieZoneApplication.dataRepository}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+        with(binding) {
+            btnFetchDetail.setOnClickListener { fetchMovieDetail() }
+            btnLoadImage.setOnClickListener { fetchMovieImage() }
+        }
+    }
 
-        lateinit var sampleMovie: MovieDetail
-        lateinit var sampleMovieImage: MovieImage
+    private fun fetchMovieImage() {
+        lateinit var movieImage: MovieImage
         lifecycleScope.launch{
             runCatching {
-                sampleMovieImage = dataRepository.getMovieImage()
-                Toast.makeText(this@MainActivity, sampleMovieImage.IMDB, Toast.LENGTH_SHORT).show()
+                movieImage = dataRepository.getMovieImage("tt1375666")
             }.onSuccess {
-                Log.i("MovieZone App", sampleMovieImage.IMDB)
-                Toast.makeText(this@MainActivity, sampleMovieImage.IMDB, Toast.LENGTH_SHORT).show()
+                Log.i("MovieZone", movieImage.toString())
+                val secureUrl = movieImage.poster.replace("http", "https")
+                binding.ivMovie.load(secureUrl)
             }.onFailure {
+                Log.i("MovieZone", "failure")
+                Toast.makeText(this@MainActivity, "Request Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun fetchMovieDetail() {
+        lateinit var sampleMovie: MovieDetail
+        lifecycleScope.launch{
+            runCatching {
+                sampleMovie = dataRepository.getMovieDetail("tt1375666")
+            }.onSuccess {
+                Log.i("MovieZone", sampleMovie.toString())
+                binding.tvTitle.text = "${sampleMovie.title} (Released in ${sampleMovie.year})"
+            }.onFailure {
+                Log.i("MovieZone", "failure")
                 Toast.makeText(this@MainActivity, "Request Failed", Toast.LENGTH_SHORT).show()
             }
         }
--- app/src/main/java/com/info448/moviezone/repository/DataRepository.kt
+++ app/src/main/java/com/info448/moviezone/repository/DataRepository.kt
@@ -6,18 +6,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 class DataRepository {
 
     val movieService = Retrofit.Builder()
-        .baseUrl("https://movies-tvshows-data-imdb.p.rapidapi.com/?")
+        .baseUrl("https://movies-tvshows-data-imdb.p.rapidapi.com/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(MovieZoneService::class.java)
 
-    suspend fun getMovieDetail(): MovieDetail = movieService.getMovieDetail()
+    suspend fun getMovieDetail(imbd_ID: String): MovieDetail = movieService.getMovieDetail(imbd_ID)
 
-    suspend fun getMovieImage(): MovieImage = movieService.getMovieImage()
+    suspend fun getMovieImage(imbd_ID: String): MovieImage = movieService.getMovieImage(imbd_ID)
 
 }
 
@@ -27,16 +28,13 @@ interface MovieZoneService {
         "x-rapidapi-key: 9dc0637ae4msh00919079d6e5317p1b66e0jsn6551d5d0f4e1",
         "x-rapidapi-host: movies-tvshows-data-imdb.p.rapidapi.com"
     )
-    @GET("type=get-movie-details&tt0245429&imdb=tt0245429")
-    suspend fun getMovieDetail(
-
-        //@Query("imdb") imdb_ID: String
-    ): MovieDetail
+    @GET("/?type=get-movie-details")
+    suspend fun getMovieDetail(@Query("imdb") imdb_ID: String): MovieDetail
 
     @Headers(
         "x-rapidapi-key: 9dc0637ae4msh00919079d6e5317p1b66e0jsn6551d5d0f4e1",
         "x-rapidapi-host: movies-tvshows-data-imdb.p.rapidapi.com"
     )
-    @GET("type=get-movies-images-by-imdb&imdb=tt1375666")
-    suspend fun getMovieImage(): MovieImage
+    @GET("/?type=get-movies-images-by-imdb")
+    suspend fun getMovieImage(@Query("imdb") imdb_ID: String): MovieImage
 }
